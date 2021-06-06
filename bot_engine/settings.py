f678ea9bb1838ebc9baee12620e1f0c4c3484a6e
LosSantosWhite <69223894+LosSantosWhite@users.noreply.github.com>
2021-06-06 12:03:31 +0300

    Changed TOKEN

--- bot_engine/settings.py
+++ bot_engine/settings.py
@@ -3,7 +3,7 @@ import parsers.royal_kids as rk
 import parsers.mir_avtokresel as mk
 import parsers.allomama as am
 import parsers.avtodetstvo as at
-from bot_token import TOKEN
+from bot_engine.bot_token import TOKEN
 
 
 def generate_message(func, errors: list) -> str:
